07730f3f0225da83af1106522aa09822990b6062
Jmeas <jellyes2@gmail.com>
2017-02-02 08:12:43 -0800

    Add comments to example/index.js

--- example/index.js
+++ example/index.js
@@ -1,3 +1,4 @@
+'use strict';
 
 const _ = require('lodash');
 const fs = require('fs');
@@ -5,8 +6,13 @@ const path = require('path');
 const yaml = require('js-yaml');
 const resourceTransform = require('../lib/resource-transform');
 
+// Store all of the resources you'd like in this directory. Eventually, these
+// would be a edited via a Web UI, or even from the command line.
 const resourceDir = path.join(__dirname, './resources');
 
+// This function reads and parses the resource from the disk, and returns it.
+// At the moment, only YAML files are supported, although it'd be simple to add
+// in support for JSON.
 function loadResource(filename) {
   const filePath = path.join(resourceDir, filename);
   const fileExt = path.extname(filePath);
@@ -23,16 +29,23 @@ function loadResource(filename) {
   return doc;
 }
 
+// Loop through all files in the directory
 const resources = fs.readdirSync(resourceDir)
+  // Open them up and parse them as JSON
   .map(loadResource)
+  // Transform them into a "resource definition," which is useful on the
+  // generated server, and a migration, which is useful for configuring the DB.
+  // Eventually, a series of migrations will be generated capturing changes
+  // made to the resource definition. Also, all of this will be stored in the
+  // DB.
   .map(resourceTransform);
 
-
+// Convert it to a JSON string so we can hand it off to the generated API.
 const resourceString = JSON.stringify(resources);
 
 // Write JSON result to stdout. This will be consumed by the server to set up
 // the routes and database.
 console.log(resourceString);
 
-// Temporarily write this out to the disk, so that we can check out our results.
+// Write this out to the disk – this is useful for debugging purposes.
 fs.writeFileSync('./test.json', resourceString);
