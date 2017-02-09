4c1aff35411f9e2de0971135900b2847891c9688
Jmeas <jellyes2@gmail.com>
2017-02-08 22:31:44 -0800

    Author basic 404 integration tests for CRUD

---
+++ test/integration/patch-resource/index.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const request = require('supertest');
+const app = require('../../../server/app');
+const getDb = require('../../../lib/database');
+const wipeDatabase = require('../../../lib/wipe-database');
+const validators = require('../../helpers/json-api-validators');
+const applyMigrations = require('../../helpers/apply-migrations');
+
+const db = getDb();
+const fixturesDirectory = path.join(__dirname, '..', '..', 'fixtures');
+
+describe('Resource PATCH', function() {
+  // Ensure that the DB connection drops immediately after each test
+  afterEach(() => {
+    db.$config.pgp.end();
+  });
+
+  // Ensure that there's no lingering data between tests by wiping the
+  // database before each test.
+  beforeEach(done => {
+    wipeDatabase(db).then(() => done());
+  });
+
+  describe('when the resource does not exist', () => {
+    it('should return a Not Found error response', (done) => {
+      const options = {
+        resourcesDirectory: path.join(fixturesDirectory, 'empty-resources')
+      };
+
+      const expectedErrors = [{
+        title: 'Resource Not Found',
+        detail: 'The requested resource does not exist.'
+      }];
+
+      applyMigrations(options)
+        .then(() => {
+          request(app(options))
+            .patch('/v1/pastas/1')
+            .expect(validators.basicValidation)
+            .expect(validators.assertErrors(expectedErrors))
+            .expect(404)
+            .end(done);
+        });
+    });
+  });
+});
