37eda6889a817be60a4fcca67581dd50d7aa1010
Jmeas <jellyes2@gmail.com>
2017-02-18 12:36:05 -0800

    Store and utilize more information on the definition

--- server/resource/middleware/create.js
+++ server/resource/middleware/create.js
@@ -16,11 +16,11 @@ module.exports = function(req, res) {
   const rawMeta = data.meta;
   const rawRelations = data.relationships;
 
-  const attrs = _.pick(rawAttrs, _.map(this.definition.attributes, 'name'));
+  const attrs = _.pick(rawAttrs, this.definition.attributeNames);
   // At the moment, this allows users to modify the built-in-meta, which is
   // no good.
-  const meta = _.pick(rawMeta, _.map(this.definition.meta, 'name'));
-  const relations = _.pick(rawRelations, _.map(this.definition.relationships, 'name'));
+  const meta = _.pick(rawMeta, this.definition.metaNames);
+  const relations = _.pick(rawRelations, this.definition.relationshipNames);
 
   // This maps the name that the user passes in to the ID that they pass in.
   // A chain().mapValue().mapKeys() could probably do this in a cleaner
