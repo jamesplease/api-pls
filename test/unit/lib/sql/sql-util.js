6b6cde9b358d20763cb08c7fa53633bf70aad9a5
Jmeas <jellyes2@gmail.com>
2017-02-18 13:41:01 -0800

    Add sqlUtil tests

---
+++ test/unit/lib/sql/sql-util.js
@@ -0,0 +1,63 @@
+const sqlUtil = require('../../../../lib/sql/sql-util');
+
+describe('sqlUtil', function() {
+  beforeEach(() => {
+    this.resource = {
+      name: 'sandwiches";'
+    };
+
+    this.oneToOneRelationship = {
+      name: 'ring',
+      resource: 'jewelry',
+      cardinality: 'one-to-one'
+    };
+
+    this.oneToManyRelationship = {
+      name: 'owner',
+      resource: 'person',
+      cardinality: 'one-to-many'
+    };
+  });
+
+  describe('getTableName', () => {
+    it('should return the unescaped name', () => {
+      assert.equal(sqlUtil.getTableName(this.resource), 'sandwiches";');
+    });
+
+    it('should return the escaped name', () => {
+      assert.equal(sqlUtil.getTableName(this.resource, {escaped: true}), '"sandwiches"";"');
+    });
+  });
+
+  describe('getIdColumnFromResource', () => {
+    it('should return the unescaped name', () => {
+      assert.equal(sqlUtil.getIdColumnFromResource(this.resource), 'id');
+    });
+
+    it('should return the escaped name', () => {
+      assert.equal(sqlUtil.getIdColumnFromResource(this.resource, {escaped: true}), '"id"');
+    });
+  });
+
+  describe('getIdSuffix', () => {
+    it('should return the right toMany suffix', () => {
+      assert.equal(sqlUtil.getIdSuffix(this.oneToManyRelationship), 'ids');
+    });
+
+    it('should return the right not-toMany suffix', () => {
+      assert.equal(sqlUtil.getIdSuffix(this.oneToOneRelationship), 'id');
+    });
+  });
+
+  describe('getRelationshipColumnName', () => {
+    it('should return the right column name', () => {
+      assert.equal(sqlUtil.getRelationshipColumnName(this.oneToManyRelationship), '"owner_ids"');
+    });
+  });
+
+  describe('getVirtualHostTableName', () => {
+    it('should return the right temporary table name', () => {
+      assert.equal(sqlUtil.getVirtualHostTableName(this.oneToManyRelationship), '"related_person_ids"');
+    });
+  });
+});
