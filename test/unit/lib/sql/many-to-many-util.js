e68ea89bd8ab70d2f675a21e576b1acc6752b238
James Smith <jellyes2@gmail.com>
2017-02-19 10:57:56 -0800

    Create associative tables in database for many-to-many relationships

---
+++ test/unit/lib/sql/many-to-many-util.js
@@ -0,0 +1,93 @@
+const manyToManyUtil = require('../../../../lib/sql/many-to-many-util');
+
+describe('manyToManyUtil', function() {
+  beforeEach(() => {
+    this.host = {
+      name: 'cat'
+    };
+
+    this.guest = {
+      name: 'person'
+    };
+  });
+
+  describe('getAssociativeTableName', () => {
+    it('should return the raw name', () => {
+      const result = manyToManyUtil.getAssociativeTableName({
+        host: this.host,
+        guest: this.guest
+      });
+
+      assert.equal(result, 'cat_person');
+    });
+
+    it('should return the escaped name', () => {
+      const result = manyToManyUtil.getAssociativeTableName({
+        host: this.host,
+        guest: this.guest,
+        escaped: true
+      });
+
+      assert.equal(result, '"cat_person"');
+    });
+  });
+
+  describe('getHostIdColumnName', () => {
+    it('should return the raw name', () => {
+      const result = manyToManyUtil.getHostIdColumnName({
+        host: this.host
+      });
+
+      assert.equal(result, 'cat_id');
+    });
+
+    it('should return the escaped name', () => {
+      const result = manyToManyUtil.getHostIdColumnName({
+        host: this.host,
+        escaped: true
+      });
+
+      assert.equal(result, '"cat_id"');
+    });
+  });
+
+  describe('getGuestIdColumnName', () => {
+    it('should return the raw name', () => {
+      const result = manyToManyUtil.getGuestIdColumnName({
+        guest: this.guest
+      });
+
+      assert.equal(result, 'person_id');
+    });
+
+    it('should return the escaped name', () => {
+      const result = manyToManyUtil.getGuestIdColumnName({
+        guest: this.guest,
+        escaped: true
+      });
+
+      assert.equal(result, '"person_id"');
+    });
+  });
+
+  describe('getPrimaryKeyColumnName', () => {
+    it('should return the raw name', () => {
+      const result = manyToManyUtil.getPrimaryKeyColumnName({
+        host: this.host,
+        guest: this.guest
+      });
+
+      assert.equal(result, 'cat_person_pkey');
+    });
+
+    it('should return the escaped name', () => {
+      const result = manyToManyUtil.getPrimaryKeyColumnName({
+        host: this.host,
+        guest: this.guest,
+        escaped: true
+      });
+
+      assert.equal(result, '"cat_person_pkey"');
+    });
+  });
+});
