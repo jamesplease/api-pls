b6d60b5fa6f2845405bd0ad704ec1194d67622fb
Jmeas <jellyes2@gmail.com>
2017-04-09 12:14:39 -0700

    Remove CLI
    It may return in the future. For now, I need to focus on the API
    around adapters.

--- packages/api-pls-cli/commands/start.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const log = require('../util/log');
-const startServer = require('../../api-pls-utils/start-server');
-
-module.exports = function(options) {
-  log.info(`Starting the API webserver on port ${options.port}`);
-  startServer(options);
-};
